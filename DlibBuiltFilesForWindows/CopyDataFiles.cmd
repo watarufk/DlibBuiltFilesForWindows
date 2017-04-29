ef991cae2d99c01b7bb3c3df0caa275950cc413a
kishimoto-exvision <kishimoto@exvision.co.jp>
2017-04-30 00:48:55 +0900

    Update ReadmeToRebuild.txt

--- DlibBuiltFilesForWindows/CopyDataFiles.cmd
+++ DlibBuiltFilesForWindows/CopyDataFiles.cmd
@@ -7,19 +7,19 @@ mkdir dlib_examples_build_x86_mkl
 
 REM Use your own downloaded files
 
-copy C:\Data\Dlib\mmod_human_face_detector.dat    dlib_examples_build_x64_avx_cuda
-copy C:\Data\Dlib\mmod_human_face_detector.dat    dlib_examples_build_x64_mkl_cuda
-copy C:\Data\Dlib\mmod_human_face_detector.dat    dlib_examples_build_x86_avx
-copy C:\Data\Dlib\mmod_human_face_detector.dat    dlib_examples_build_x86_mkl
+copy D:\Data\Dlib\mmod_human_face_detector.dat    dlib_examples_build_x64_avx_cuda
+copy D:\Data\Dlib\mmod_human_face_detector.dat    dlib_examples_build_x64_mkl_cuda
+copy D:\Data\Dlib\mmod_human_face_detector.dat    dlib_examples_build_x86_avx
+copy D:\Data\Dlib\mmod_human_face_detector.dat    dlib_examples_build_x86_mkl
 
-copy C:\Data\Dlib\resnet34_1000_imagenet_classifier.dnn    dlib_examples_build_x64_avx_cuda
-copy C:\Data\Dlib\resnet34_1000_imagenet_classifier.dnn    dlib_examples_build_x64_mkl_cuda
-copy C:\Data\Dlib\resnet34_1000_imagenet_classifier.dnn    dlib_examples_build_x86_avx
-copy C:\Data\Dlib\resnet34_1000_imagenet_classifier.dnn    dlib_examples_build_x86_mkl
+copy D:\Data\Dlib\resnet34_1000_imagenet_classifier.dnn    dlib_examples_build_x64_avx_cuda
+copy D:\Data\Dlib\resnet34_1000_imagenet_classifier.dnn    dlib_examples_build_x64_mkl_cuda
+copy D:\Data\Dlib\resnet34_1000_imagenet_classifier.dnn    dlib_examples_build_x86_avx
+copy D:\Data\Dlib\resnet34_1000_imagenet_classifier.dnn    dlib_examples_build_x86_mkl
 
-copy C:\Data\Dlib\shape_predictor_68_face_landmarks.dat    dlib_examples_build_x64_avx_cuda
-copy C:\Data\Dlib\shape_predictor_68_face_landmarks.dat    dlib_examples_build_x64_mkl_cuda
-copy C:\Data\Dlib\shape_predictor_68_face_landmarks.dat    dlib_examples_build_x86_avx
-copy C:\Data\Dlib\shape_predictor_68_face_landmarks.dat    dlib_examples_build_x86_mkl
+copy D:\Data\Dlib\shape_predictor_68_face_landmarks.dat    dlib_examples_build_x64_avx_cuda
+copy D:\Data\Dlib\shape_predictor_68_face_landmarks.dat    dlib_examples_build_x64_mkl_cuda
+copy D:\Data\Dlib\shape_predictor_68_face_landmarks.dat    dlib_examples_build_x86_avx
+copy D:\Data\Dlib\shape_predictor_68_face_landmarks.dat    dlib_examples_build_x86_mkl
 
 pause
--- DlibBuiltFilesForWindows/ReadmeToRebuild.txt
+++ DlibBuiltFilesForWindows/ReadmeToRebuild.txt
@@ -2,12 +2,25 @@ Update submodules
 https://github.com/davisking/dlib.git
 at first.  And then run CallingCmake_dlib.cmd.
 
-In some dlib_examples_build_x86_mkl projects, you need to set "Use Intel MKL" to "Parallel" in project property screen.
- * If you use "Debug" configuration and you don't set it, building the programs failes.
-
 
 [NOTE]
 ../../opencv_{x86|x64}/install is set to OpenCV_DIR:PATH in dlib cmake settings.
 I set BUILD_opencv_world=OFF, because OpenCV_LIBS (=OpenCV_LIB_COMPONENTS) contains both of each libary and opencv_world, so link errors occurs.
 
 When you make your own project files which uses dnn_*.cpp files, you need to add project property "/bigobj" in [Propety Pages] - [C/C++] - [Command Line] - [Additional Options].
+
+About Intel MKL, dlib uses static library files.
+https://software.intel.com/en-us/articles/intel-math-kernel-library-intel-mkl-linkage-and-distribution-quick-reference-guide
+"Static library, contains all processor-specific optimization in one package.
+mkl_intel_c.lib
+mkl_intel_thread.lib
+mkl_core.lib
+..."
+
+But libiomp5md.dll is NOT static linked, so you need to register paths for libiomp5md.dll or copy it into the same folder as EXE files.
+* C:\Program Files (x86)\IntelSWTools\compilers_and_libraries_2017.2.187\windows\redist\ia32\compiler
+* C:\Program Files (x86)\IntelSWTools\compilers_and_libraries_2017.2.187\windows\redist\intel64\compiler
+
+Each project which uses DlibExtern.dll, you can set [Project Property Pages] - [Intel Performance Libraries] - [Use Intel MKL] to "No".
+
+In some cases, it seems to be necessary to set [Linker] - [Input] - [Ignore Specific Default Libraries] to "libcpmt.lib;%(IgnoreSpecificDefaultLibraries)".
