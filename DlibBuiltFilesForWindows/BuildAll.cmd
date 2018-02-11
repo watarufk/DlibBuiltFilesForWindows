ab0dfddf476d79cc0fcc07afd4491d0b05e1d783
kishimoto-exvision <kishimoto@exvision.co.jp>
2018-02-11 17:08:06 +0900

    Rename CopyDataFiles.cmd to DownloadAndCopyDataFiles.cmd

--- DlibBuiltFilesForWindows/BuildAll.cmd
+++ DlibBuiltFilesForWindows/BuildAll.cmd
@@ -1,4 +1,5 @@
 cd %~dp0..
+
 rmdir /S /Q dlib_examples_build_x64_mkl_cuda
 rmdir /S /Q dlib_examples_build_x64_avx_cuda
 rmdir /S /Q dlib_examples_build_x86_mkl
@@ -8,7 +9,7 @@ call %~dp0Build_dlib_examples_x64_mkl_cuda.cmd
 call %~dp0Build_dlib_examples_x64_avx_cuda.cmd
 call %~dp0Build_dlib_examples_x86_mkl.cmd
 
-call %~dp0CopyDataFiles.cmd
+call %~dp0DownloadAndCopyDataFiles.cmd
 call %~dp0CopyOpenCvDlls.cmd
 
 pause
