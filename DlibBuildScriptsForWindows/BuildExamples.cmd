55775a5fba474683235d2dc2ae23d250cd5541d7
kishimoto-exvision <kishimoto@exvision.co.jp>
2018-02-11 20:55:47 +0900

    Fib a bug in BuildExamples.cmd

--- DlibBuildScriptsForWindows/BuildExamples.cmd
+++ DlibBuildScriptsForWindows/BuildExamples.cmd
@@ -7,10 +7,6 @@ rmdir /S /Q dlib_examples_x64_mkl_cuda
 rmdir /S /Q dlib_examples_x64_avx_cuda
 rmdir /S /Q dlib_examples_x86_mkl
 
-cd %~dp0..
 call %~dp0Build_dlib_examples_x64_mkl_cuda.cmd
 call %~dp0Build_dlib_examples_x64_avx_cuda.cmd
 call %~dp0Build_dlib_examples_x86_mkl.cmd
-
-call %~dp0DownloadAndCopyDataFiles.cmd
-call %~dp0CopyOpenCvDlls.cmd
