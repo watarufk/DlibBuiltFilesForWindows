ade0eb8aff6b9c09c5206ce173b031eaf06292f7
kishimoto-exvision <kishimoto@exvision.co.jp>
2018-02-11 17:03:51 +0900

    Fix CMake arguments in build scripts

--- DlibBuiltFilesForWindows/Build_dlib_examples_x64_avx_cuda.cmd
+++ DlibBuiltFilesForWindows/Build_dlib_examples_x64_avx_cuda.cmd
@@ -6,6 +6,18 @@ cd dlib_examples_build_x64_avx_cuda
 
 rem Free version MKL can be used from OSS.
 cmake.exe ../dlib/examples -A x64 ^
+  -DOpenCV_DIR:PATH="%~dp0../opencv_x64/install" ^
+  -DJPEG_INCLUDE_DIR:PATH="" ^
+  -DJPEG_LIBRARY:FILEPATH="" ^
+  -DOpenCV_DIR:PATH="%~dp0../opencv_x64/install" ^
+  -DPNG_LIBRARY_DEBUG:FILEPATH="" ^
+  -DPNG_LIBRARY_RELEASE:FILEPATH="" ^
+  -DPNG_PNG_INCLUDE_DIR:PATH="" ^
+  -DZLIB_INCLUDE_DIR:PATH="" ^
+  -DZLIB_LIBRARY_DEBUG:FILEPATH="" ^
+  -DZLIB_LIBRARY_RELEASE:FILEPATH="" ^
+  -DLIBJPEG_IS_GOOD:INTERNAL="" ^
+  -DLIBPNG_IS_GOOD:INTERNAL="" ^
   -DUSE_AVX_INSTRUCTIONS:BOOL="1" ^
   -Dmkl_core="" ^
   -Dmkl_intel="" ^
--- DlibBuiltFilesForWindows/Build_dlib_examples_x64_mkl_cuda.cmd
+++ DlibBuiltFilesForWindows/Build_dlib_examples_x64_mkl_cuda.cmd
@@ -6,6 +6,18 @@ cd dlib_examples_build_x64_mkl_cuda
 
 rem You need to install MKL on your build PC.
 cmake.exe ../dlib/examples -A x64 ^
+  -DOpenCV_DIR:PATH="%~dp0../opencv_x64/install" ^
+  -DJPEG_INCLUDE_DIR:PATH="" ^
+  -DJPEG_LIBRARY:FILEPATH="" ^
+  -DOpenCV_DIR:PATH="%~dp0../opencv_x64/install" ^
+  -DPNG_LIBRARY_DEBUG:FILEPATH="" ^
+  -DPNG_LIBRARY_RELEASE:FILEPATH="" ^
+  -DPNG_PNG_INCLUDE_DIR:PATH="" ^
+  -DZLIB_INCLUDE_DIR:PATH="" ^
+  -DZLIB_LIBRARY_DEBUG:FILEPATH="" ^
+  -DZLIB_LIBRARY_RELEASE:FILEPATH="" ^
+  -DLIBJPEG_IS_GOOD:INTERNAL="" ^
+  -DLIBPNG_IS_GOOD:INTERNAL="" ^
   -DUSE_AVX_INSTRUCTIONS:BOOL="1"
 
 cmake.exe --build "." --target "ALL_BUILD" --config "Release"
